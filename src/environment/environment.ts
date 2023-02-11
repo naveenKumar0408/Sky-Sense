2bb83ef848fb7645a5b02d765ac3e8518c77e895
naveenKumar0408 <naveenkumar19981119@gmail.com>
2023-02-11 15:42:19 +0530

    Add Weather Data Code and Service

---
+++ src/environment/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'a11c1b6160msh83a31e7326f80e4p12c873jsnd4e251d7afb7',
+};

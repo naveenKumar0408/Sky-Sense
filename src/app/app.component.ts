9cb7eff37a55ebf61c19c6bafe6b5b46342e1580
naveenKumar0408 <naveenkumar19981119@gmail.com>
2023-02-11 15:58:29 +0530

    Update static code with Dynamic Weather Data from API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'SkySense';
+export class AppComponent implements OnInit {
+  weatherData?: WeatherData;
+  constructor(private weatherService: WeatherService) {}
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Bengaluru').subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response);
+      },
+    });
+  }
 }
